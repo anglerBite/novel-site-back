48012af4cfb42de0cf8ad0680e2f720a81ad4a20
anglerBite <jackall489@gmail.com>
2024-06-12 11:40:58 +0900

    a

--- server.js
+++ server.js
@@ -15,13 +15,6 @@ app.use(cors());
 app.use('/novel', routes);
 app.use('/login', login);
 
-//追加
-app.use(express.static(path.join(__dirname, 'frontend','build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-});
-
 //データベース接続とサーバー起動
 const connectDB = async () => {
     try {
